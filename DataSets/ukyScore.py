eaa814814ab2c7d608cb0eadb26459d69ac52966
briansdumb <bevo.davis@gmail.com>
2019-03-21 10:59:14 -0400

    new metric

--- DataSets/ukyScore.py
+++ DataSets/ukyScore.py
@@ -258,6 +258,7 @@ class data_set:
                     scores = validPop.apply(lambda x: self.objectiveFunction(x)[0], axis=1)
                     meanScore = np.mean(scores.values)
                     minScore = np.min(scores.values)
+
                     if numUnique == 1:
                         var = 0.0
                     else:
@@ -331,4 +332,82 @@ splits = data.geneticOptimizer(numGens=1000, printFreq=50, POPSIZE=1000, scoreGo
 from importlib import reload
 reload(ukyScore) 
 
+
+t0 = time()
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            # Create optimizer tools
+            creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
+            creator.create("Individual", np.ndarray, fitness=creator.FitnessMin)
+            toolbox = base.Toolbox()
+            toolbox.register("attr_bool", np.random.choice, 2, p=[1 - data.targetRatio, data.targetRatio])
+            toolbox.register("individual", tools.initRepeat, creator.Individual, toolbox.attr_bool, data.size)
+            toolbox.register("population", tools.initRepeat, list, toolbox.individual)
+            toolbox.register("evaluate", data.objectiveFunction)
+            toolbox.register("mate", tools.cxOnePoint)
+            toolbox.register("mutate", tools.mutFlipBit, indpb=INDPB)
+            toolbox.register("select", tools.selTournament, tournsize=TOURNSIZE)
+        # Set random seed for reproducibility
+        np.random.seed(42)
+        random.seed(42)
+        pop = toolbox.population(n=POPSIZE)
+        fitnesses = list(map(toolbox.evaluate, pop))
+        for ind, fit in zip(pop, fitnesses):
+            ind.fitness.values = fit
+        gen = 0
+        minScore = 2.0
+        while gen < numGens and scoreGoal < minScore:
+            # Select the next generation individuals
+            offspring = toolbox.select(pop, len(pop))
+            # Clone the selected individuals
+            offspring = list(map(toolbox.clone, offspring))
+            # Apply crossover and mutation on the offspring
+            for child1, child2 in zip(offspring[::2], offspring[1::2]):
+                if random.random() < CXPB:
+                    toolbox.mate(child1, child2)
+                    del child1.fitness.values
+                    del child2.fitness.values
+            for mutant in offspring:
+                if random.random() < MUTPB:
+                    toolbox.mutate(mutant)
+                    del mutant.fitness.values
+            # Evaluate the individuals with an invalid fitness
+            invalid_ind = [ind for ind in offspring if not ind.fitness.valid]
+            fitnesses = map(toolbox.evaluate, invalid_ind)
+            for ind, fit in zip(invalid_ind, fitnesses):
+                ind.fitness.values = fit
+            pop[:] = offspring
+            if gen % printFreq == 0:
+                Pop = pd.DataFrame(pop)
+                validPop = Pop[Pop.apply(lambda x: data.validSplit(x), axis=1)]
+                validPop = validPop.drop_duplicates()
+                numUnique = len(validPop)
+                if numUnique == 0:
+                    meanScore = np.nan
+                    minScore = np.nan
+                    var = np.nan
+                else:
+                    scores = validPop.apply(lambda x: data.objectiveFunction(x)[0], axis=1)
+                    meanScore = np.mean(scores.values)
+                    minScore = np.min(scores.values)
+                    optSplit = 
+                    if numUnique == 1:
+                        var = 0.0
+                    else:
+                        var = validPop.var().mean()
+                if verbose:
+                    print('-- Generation {}'.format(gen)
+                          + ' -- Time (sec): {}'.format(np.round((time() - t0), 2))
+                          + ' -- Min score: {}'.format(np.round(minScore, 4))
+                          + ' -- Mean score: {}'.format(np.round(meanScore, 4))
+                          + ' -- Unique Valid splits: {}/{}'.format(numUnique, POPSIZE)
+                          + ' -- Var splits: {}'.format(np.round(var, 4))
+                          )
+                else:
+                    print('-- Generation {}'.format(gen)
+                          + ' -- Time (sec): {}'.format(np.round((time() - t0), 2))
+                          + ' -- Min score: {}'.format(np.round(minScore, 4))
+                          )
+                data.optRecord.append((time() - t0, data.computeScores()))
+            gen += 1
 """
