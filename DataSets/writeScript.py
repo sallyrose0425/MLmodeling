ea1f03d839c31395b850fe95c1ce874377745e38
briansdumb <bevo.davis@gmail.com>
2019-03-22 15:27:21 -0400

    new metric

--- DataSets/writeScript.py
+++ DataSets/writeScript.py
@@ -6,14 +6,12 @@ from glob import glob
 dataset = 'dekois'
 files = glob(os.getcwd() + '/DataSets/' + dataset + '/decoys/*.sdf.gz')
 
-n = 1
-calls = ['# !/bin/bash\n\n']
-for file in files[16*n:16*(n+1)-1]:
+calls = ['# !/bin/bash\ntsp -S 16\n']
+for file in files:
     target_id = file.split('/')[-1].split('_')[0]
-    call = f'python singleTargetProcess.py {dataset} {target_id} & \n'
+    call = f'tsp python singleTargetProcess.py {dataset} {target_id} \n'
     calls.append(call)
-target_id = files[16*(n+1)].split('/')[-1].split('_')[0]
-calls.append(f'python singleTargetProcess.py {dataset} {target_id}')
+
 
 f = open(os.getcwd() + '/DataSets/runScript', 'w+')
 f.writelines(calls)
