775af964277fd3fb239d5f149de2308e2c0c75ce
briansdumb <bevo.davis@gmail.com>
2019-03-25 12:10:40 -0400

    new metric

--- DataSets/writeScript.py
+++ DataSets/writeScript.py
@@ -2,12 +2,16 @@ import os
 from glob import glob
 
 
-dataset = 'DUDE'
+dataset = 'dekois'
 prefix = os.getcwd() + '/DataSets/' + dataset + '/'
 files = glob(os.getcwd() + '/DataSets/' + dataset + '/*')
+targets = []
 sizes = []
 for file in files:
     target_id = file.split('/')[-1].split('_')[0]
+    targets.append(target_id)
+targets = list(set(targets))
+for target_id in targets:
     if dataset == 'dekois':
         activeFile = prefix + 'ligands/' + target_id + '.sdf.gz'
         decoyFile = prefix + 'decoys/' + target_id + '_Celling-v1.12_decoyset.sdf.gz'
@@ -27,7 +31,7 @@ sizes.sort()
 calls = ['# !/bin/bash\ntsp -S 16\n']
 for pair in sizes:
     target_id = pair[1]
-    call = f'tsp python singleTargetProcess.py {dataset} {target_id} \n'
+    call = f'tsp python evaluation.py {dataset} {target_id} \n'
     calls.append(call)
 
 f = open(os.getcwd() + '/DataSets/runScript', 'w+')
