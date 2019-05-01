c47165c963981d0ba402ce84f9c2f6072f615fbb
briansdumb <bevo.davis@gmail.com>
2019-05-01 17:55:47 -0400

    new metric

--- singleTargetProcess.py
+++ singleTargetProcess.py
@@ -38,7 +38,7 @@ def distToNN(probs, nn):
 
 def main(dataset, target_id, atomwise, popSize, tournsize, cxpb, mutpb, indpb, one_point):
     ATOMWISE = (atomwise == 'ATOMWISE=True')
-    prefix = os.getcwd() + '/DataSets/' + dataset + '/'
+    prefix = os.getcwd() + '/' + dataset + '/'
     if dataset == 'dekois':
         activeFile = prefix + 'ligands/' + target_id + '.sdf.gz'
         decoyFile = prefix + 'decoys/' + target_id + '_Celling-v1.12_decoyset.sdf.gz'
